faf857b1eca6d0e5ee1748b47e27df007273ebe3
vansjyo <vanshika421@gmail.com>
2019-01-29 22:37:34 +0530

    added extension

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render,get_object_or_404
 from .models import Post #The dot before models means current directory or current application
 
 
@@ -7,3 +8,8 @@ from .models import Post #The dot before models means current directory or curre
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+	#Note that we need to use exactly the same name as the one we specified in urls (pk). Omitting this variable is incorrect and will result in an error!
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
